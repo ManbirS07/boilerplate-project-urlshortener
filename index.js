53a121f28ba60b7119ecb518d351ed2fe2984c8d
ManbirS07 <msmanbir26@gmail.com>
2025-01-25 02:20:40 +0530

    Done

--- index.js
+++ index.js
@@ -2,17 +2,52 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser=require('body-parser')
+var validUrl = require('valid-url');
+const shortid=require("shortid")
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(cors());
+
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+//code
+
+const urlstore={}
+app.post('/api/shorturl',function(req,res)
+{
+  const originalurl=req.body.url
+  console.log(originalurl)
+  if(!validUrl.isUri(originalurl) || !originalurl.startsWith('http://') && !originalurl.startsWith('https://'))
+  {
+    return res.json({error:'invalid url'})
+  }
+  const shorturl=shortid.generate()
+  urlstore[shorturl]=originalurl
+  res.json({original_url:originalurl,short_url:shorturl})
+})
+
+app.get('/api/shorturl/:short',function(req,res)
+{
+  const short=req.params.short
+  if (urlstore.hasOwnProperty(short)) {
+    const original = urlstore[short]; 
+    res.redirect(original); 
+  } 
+  else 
+  {
+    res.json({ error: 'invalid url' }); 
+  }
+})
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
@@ -22,3 +57,4 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
